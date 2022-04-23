1511bc92fa8d70f9e0a3a6670ac036a7a432e80a
4b <4b@INF18>
2022-04-23 11:49:30 +0200

    versione 1

---
+++ src/Principale.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author steven
+ * @version 1.0
+ */
+public class Principale {
+	public static void main(String[]args)
+	{
+		System.out.println("EL GOBLINO");
+		/*4b_prova1
+		 * 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
+		   	https://github.com/StevenMolle/4b_prova1.git*/
+	}
+}
